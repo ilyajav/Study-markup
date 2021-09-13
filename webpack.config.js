e8e4f650d26ed511f92e7a8cade0056c4bbfc227
Ilya Yavorovskiy <ilia777824@gmail.com>
2021-09-13 15:37:53 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(s*)css$/,
+                use: [
+                    miniCss.loader,
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            }
+        ]
+    },
+    plugins: [
+        new miniCss({
+            filename: 'style.css',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+};
